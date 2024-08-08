0fa75299ba4005dc323707b844bfd46d027c4d88
Daniil Shabalin <modowhen@gmail.com>
2024-08-08 17:01:56 +0300

    Добавленное исправление

--- main/java/jm/task/core/jdbc/Main.java
+++ main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,23 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();
+        userService.dropUsersTable();
+        userService.createUsersTable();
+        userService.saveUser("Daniil", "Shabalin", (byte) 23);
+        userService.saveUser("Nikolay", "Shabalin", (byte) 14);
+        userService.saveUser("Viktor", "Shabalin", (byte) 60);
+        userService.saveUser("Aleksey", "Shabalin", (byte) 44);
+        userService.removeUserById(2);
+        for (User user: userService.getAllUsers()){
+            System.out.println(user);// реализуйте алгоритм здесь
+        }
+        userService.cleanUsersTable();
     }
 }
